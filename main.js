8d46977ebb6372135f6582f9d161a2e7daea09cf
invokecreate <59404770+invokecreate@users.noreply.github.com>
2020-06-11 18:05:50 -0500

    Removed initStyle function

--- main.js
+++ main.js
@@ -39,62 +39,6 @@ $.getJSON("YOUR JSON RESOURCE PAGE", function(json) {
                 }
             });
         },
-        initDocChart: function() {
-            chartColor = "#FFFFFF";
-            // General Configuration For The Chart GradientStroke
-            gradientChartOptionsConfiguration = {
-                maintainAspectRatio: false,
-                legend: {
-                    display: false
-                },
-                tooltips: {
-                    bodySpacing: 4,
-                    mode: "nearest",
-                    intersect: 0,
-                    position: "nearest",
-                    xPadding: 10,
-                    yPadding: 10,
-                    caretPadding: 10
-                },
-                responsive: true,
-                scales: {
-                    yAxes: [{
-                        display: 0,
-                        gridLines: 0,
-                        ticks: {
-                            display: false
-                        },
-                        gridLines: {
-                            zeroLineColor: "transparent",
-                            drawTicks: false,
-                            display: false,
-                            drawBorder: false
-                        }
-                    }],
-                    xAxes: [{
-                        display: 0,
-                        gridLines: 0,
-                        ticks: {
-                            display: false
-                        },
-                        gridLines: {
-                            zeroLineColor: "transparent",
-                            drawTicks: false,
-                            display: false,
-                            drawBorder: false
-                        }
-                    }]
-                },
-                layout: {
-                    padding: {
-                        left: 0,
-                        right: 0,
-                        top: 15,
-                        bottom: 15
-                    }
-                }
-            };
-        },
         /* Initialize Chart */
         initDashboardPageCharts: function() {
             gradientChartOptionsConfigurationWithTooltipPurple = {
